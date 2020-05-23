35b63fc26859667a509063dd5aed12981f6d8e32
Phill McGee <entrenest@outlook.com>
2020-05-23 21:24:21 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-row_1 = puts "   |   |   "
-row_2 = puts "-----------"
-
 def display_board
-  row_1
-  row_2
-  row_1
-  row_2
-  row_1
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
